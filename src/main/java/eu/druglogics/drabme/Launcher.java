c96fabec7ac0614f06bf6d7cc359e7e26c9847e5
john <bblodfon@gmail.com>
2019-04-03 16:04:10 +0200

    modified Launcher to use fewer parameters and use the new commandLineArgs.java

--- src/main/java/eu/druglogics/drabme/Launcher.java
+++ src/main/java/eu/druglogics/drabme/Launcher.java
@@ -1,5 +1,14 @@
 package eu.druglogics.drabme;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+import eu.druglogics.drabme.input.CommandLineArgs;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import static eu.druglogics.gitsbe.util.Util.*;
 
 public class Launcher {
@@ -16,47 +25,53 @@ public class Launcher {
 	}
 
 	private void setupInputAndRun(String[] args) {
-		if (args.length == 0) {
-			System.out.println("No user arguments supplied");
-			System.out.println(
-					"Usage: drabme <name project> <directory models> <filename drugs> <filename model outputs> "
-							+ "<filename combinations> <directory output> <preserveTmpFiles> <directory tmp> <verbosity>");
 
-			System.out.println("\nExample Testrun (command line usage): ");
+		try {
+			CommandLineArgs arguments = new CommandLineArgs();
+			JCommander.newBuilder().addObject(arguments).build().parse(args);
 
-			args = new String[] { "example_run_ags", "directoryOutput/models", "toy_ags_drugpanel.tab",
-					"toy_ags_modeloutputs.tab", "toy_ags_perturbations.tab", "directoryOutput", "false",
-					"directoryOutput/drabme_tmp", "verbosity" };
-			System.out.println(
-					"java -cp /pathToBinFolderOfDrabmeProject:/pathToBinFolderOfGitsbeProject:lib/combinatoricslib-2.1.jar:lib/commons-math3-3.4.1.jar drabme.Launcher "
-							+ args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]
-							+ " " + args[6] + " " + args[7] + " " + args[8] + "\n\n");
-		} else {
-			String projectName = args[0];
-			String directoryModels = args[1];
-			String filenameDrugs = args[2];
-			String filenameModelOutputs = args[3];
-			String filenameCombinations = args[4];
-			String directoryOutput = args[5];
-			boolean preserveTmpFiles = Boolean.parseBoolean(args[6]);
-			String directoryTmp = args[7];
-			int verbosity = Integer.parseInt(args[8]);
-			
-			directoryTmp = makeDirectoryPathAbsolute(directoryTmp);
-			directoryOutput = makeDirectoryPathAbsolute(directoryOutput);
-			
-			int combosize = 2;
-			Thread t;
+			String projectName = arguments.getProjectName();
+			String directoryModels = arguments.getDirectoryModels();
+			String filenameDrugs = arguments.getFilenameDrugs();
+			String filenamePerturbations = arguments.getFilenamePerturbations();
+			String filenameConfig = arguments.getFilenameConfig();
+			String filenameModelOutputs = arguments.getFilenameModelOutputs();
 
-			t = new Thread(new Drabme(verbosity, projectName, directoryModels, filenameDrugs, filenameCombinations,
-					filenameModelOutputs, directoryOutput, directoryTmp, preserveTmpFiles, combosize));
-			t.start();
+			// Inferring the input directory from the config file
+			String directoryInput = inferInputDir(filenameConfig);
+
+			// projectName is not required, but we set it either way
+			if (projectName == null) {
+				projectName = getFileName(directoryInput);
+			}
+
+			DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+			String directoryOutput = new File(directoryInput,
+					projectName + "_" + dateFormat.format(Calendar.getInstance().getTime()))
+					.getAbsolutePath();
+			String directoryTmp = new File(directoryOutput, "drabme_tmp").getAbsolutePath();
+
+			Thread thread = new Thread(new Drabme(
+					projectName,
+					filenameDrugs,
+					filenamePerturbations,
+					filenameModelOutputs,
+					filenameConfig,
+					directoryModels,
+					directoryOutput,
+					directoryTmp
+			));
+			thread.start();
 			try {
-				t.join();
+				thread.join();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 				System.exit(1);
 			}
+		} catch (ParameterException parEx) {
+			System.out.println("\nOptions preceded by an asterisk are required.");
+			parEx.getJCommander().setProgramName("eu.druglogics.drabme.Launcher");
+			parEx.usage();
 		}
 	}
 }
