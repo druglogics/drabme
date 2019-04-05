944775158e1154801ce53c0d61a92296925b7ae0
john <bblodfon@gmail.com>
2019-04-05 17:33:09 +0200

    updated to Junit5

--- src/test/java/eu/druglogics/drabme/drug/DrugTest.java
+++ src/test/java/eu/druglogics/drabme/drug/DrugTest.java
@@ -1,68 +1,70 @@
 package eu.druglogics.drabme.drug;
 
 import eu.druglogics.gitsbe.util.Logger;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
-public class DrugTest {
+class DrugTest {
 
     private Drug testDrug;
 
-    @Before
-    public void init() {
+    @BeforeEach
+    void init() {
         Logger mockLogger = mock(Logger.class);
         this.testDrug = new Drug("PD", mockLogger);
     }
 
     @Test
-    public void test_add_effect() {
+    void test_add_effect() {
         testDrug.addEffect(false);
-        Assert.assertFalse(testDrug.getEffect());
+        assertFalse(testDrug.getEffect());
 
         testDrug.addEffect(true);
-        Assert.assertTrue(testDrug.getEffect());
+        assertTrue(testDrug.getEffect());
     }
 
     @Test
-    public void test_add_empty_targets() {
+    void test_add_empty_targets() {
         String[] testTargets = {};
         testDrug.addTargets(testTargets);
 
-        Assert.assertTrue(testDrug.getTargets().isEmpty());
+        assertTrue(testDrug.getTargets().isEmpty());
     }
 
     @Test
-    public void test_add_non_empty_targets() {
+    void test_add_non_empty_targets() {
         String [] testTargets = {"MAP2K1", "MAP2K2"};
         testDrug.addTargets(testTargets);
 
         ArrayList<String> targets = testDrug.getTargets();
 
-        Assert.assertFalse(targets.isEmpty());
-        Assert.assertEquals(targets.size(), 2);
-        Assert.assertEquals(targets.get(0), "MAP2K1");
-        Assert.assertEquals(targets.get(1), "MAP2K2");
+        assertFalse(targets.isEmpty());
+        assertEquals(targets.size(), 2);
+        assertEquals(targets.get(0), "MAP2K1");
+        assertEquals(targets.get(1), "MAP2K2");
     }
 
     @Test
-    public void test_remove_target() {
+    void test_remove_target() {
         String [] testTargets = {"MAP2K1", "MAP2K2"};
         testDrug.addTargets(testTargets);
         testDrug.removeTarget("MAP"); // non-existent target
 
         ArrayList<String> targets = testDrug.getTargets();
 
-        Assert.assertEquals(targets.size(), 2);
-        Assert.assertEquals(targets.get(0), "MAP2K1");
-        Assert.assertEquals(targets.get(1), "MAP2K2");
+        assertEquals(targets.size(), 2);
+        assertEquals(targets.get(0), "MAP2K1");
+        assertEquals(targets.get(1), "MAP2K2");
 
         testDrug.removeTarget("MAP2K1");
-        Assert.assertEquals(targets.size(), 1);
-        Assert.assertEquals(targets.get(0), "MAP2K2");
+        assertEquals(targets.size(), 1);
+        assertEquals(targets.get(0), "MAP2K2");
     }
 }
--- src/test/java/eu/druglogics/drabme/input/CommandLineArgsTest.java
+++ src/test/java/eu/druglogics/drabme/input/CommandLineArgsTest.java
@@ -2,47 +2,46 @@ package eu.druglogics.drabme.input;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
-public class CommandLineArgsTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CommandLineArgsTest {
 
     @Test
-    public void test_all_arguments() {
+    void test_all_arguments() {
         CommandLineArgs args = new CommandLineArgs();
 
         String[] argv = {"-p", "project", "-md", "modelsDir", "-dr", "drugs",
                 "-c", "config", "-m", "outputs"};
         JCommander.newBuilder().addObject(args).build().parse(argv);
 
-        Assert.assertEquals(args.getProjectName(), "project");
-        Assert.assertEquals(args.getDirectoryModels(), "modelsDir");
-        Assert.assertEquals(args.getFilenameDrugs(), "drugs");
-        Assert.assertEquals(args.getFilenameConfig(), "config");
-        Assert.assertEquals(args.getFilenameModelOutputs(), "outputs");
-        Assert.assertNull(args.getFilenamePerturbations());
-    }
-
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
-
-    @Test
-    public void test_no_arguments() {
-        exception.expect(ParameterException.class);
-
-        CommandLineArgs args = new CommandLineArgs();
-        JCommander.newBuilder().addObject(args).build().parse("");
+        assertEquals(args.getProjectName(), "project");
+        assertEquals(args.getDirectoryModels(), "modelsDir");
+        assertEquals(args.getFilenameDrugs(), "drugs");
+        assertEquals(args.getFilenameConfig(), "config");
+        assertEquals(args.getFilenameModelOutputs(), "outputs");
+        assertNull(args.getFilenamePerturbations());
     }
 
     @Test
-    public void test_missing_required_arguments() {
-        exception.expect(ParameterException.class);
+    void test_no_arguments() {
+        assertThrows(ParameterException.class, () -> {
+            CommandLineArgs args = new CommandLineArgs();
+            JCommander.newBuilder().addObject(args).build().parse("");
+        },"");
+    }
 
-        CommandLineArgs args = new CommandLineArgs();
-        String[] argv = {"-p", "project", "-c", "config"};
-        JCommander.newBuilder().addObject(args).build().parse(argv);
+    @Test
+    void test_missing_required_arguments() {
+        assertThrows(ParameterException.class, () -> {
+            CommandLineArgs args = new CommandLineArgs();
+            String[] argv = {"-p", "project", "-c", "config"};
+            JCommander.newBuilder().addObject(args).build().parse(argv);
+        },"");
     }
 
 }
